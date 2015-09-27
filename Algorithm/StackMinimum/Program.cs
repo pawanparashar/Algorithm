47bb5606176e4b376fdd52248e257eedfb183855
pawan227@gmail.com <pawan227@gmail.com>
2015-09-27 16:57:32 -0700

    Stack Minimum

---
+++ Algorithm/StackMinimum/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackMinimum
+{
+    class Program
+    {
+
+        Stack<int> RealStacker = new Stack<int>();
+        Stack<int> MinStacker = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+
+            Program P = new Program();
+
+            P.CustomPush(1);
+            P.CustomPush(200);
+            P.CustomPush(3);
+            P.CustomPush(400);
+            P.CustomPush(1);
+            P.CustomPush(0);
+
+        }
+
+        public void CustomPush(int Content)
+        {
+
+            RealStacker.Push(Content);
+            if(MinStacker.Count ==0)
+            {
+                MinStacker.Push(Content);
+            }
+            else
+            {
+                if(MinStacker.Peek() <=Content)
+                {
+                    MinStacker.Push(MinStacker.Peek());
+                }
+                else
+                {
+                    MinStacker.Push(Content);
+                }
+            }
+        }
+
+
+        public void CustomPop()
+        {
+
+            RealStacker.Pop();
+            MinStacker.Pop();
+        }
+
+    }
+}
