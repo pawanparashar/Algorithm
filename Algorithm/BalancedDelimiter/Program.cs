5addb3f52e105be8f8ed5cb37eb660d9f2c283b3
pawan227@gmail.com <pawan227@gmail.com>
2015-09-26 19:21:32 -0700

    Copied the Project

--- Algorithm/BalancedDelimiter/Program.cs
+++ Algorithm/BalancedDelimiter/Program.cs
@@ -10,6 +10,51 @@ namespace BalancedDelimiter
     {
         static void Main(string[] args)
         {
+            Program P = new Program();
+            if (P.BalancedDelimiter("(){}[]"))
+            {
+
+                Console.WriteLine("It is Balanced Delimiter");
+            }
+            else
+            {
+                Console.WriteLine("It is Not Balanced Delimiter");
+
+            }
+        }
+
+
+        public bool BalancedDelimiter(String input)
+        {
+            Stack<char> Stacker = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(' || input[i] == '{' || input[i] == '[')
+                {
+                    Stacker.Push(input[i]);
+                }
+                else if (input[i] == ')' || input[i] == '}' || input[i] == ']')
+                {
+                    if (Stacker.Peek() == '(' || Stacker.Peek() == '[' || Stacker.Peek() == '{')
+                    {
+                        Stacker.Pop();
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Char given is not Opening or closing brackets");
+
+                }
+
+            }
+
+            if (Stacker.Count > 0)
+                return false;
+            else
+                return true;
+
         }
     }
 }
---
+++ Algorithm/towerofhanoi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace towerofhanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            P.towerofhanoi(5, "Source", "temp", "Destination");
+        }
+
+        public void towerofhanoi(int n, string Source, string temp, string Destination)
+        {
+            if (n == 0)
+            {
+                return;
+
+            }
+
+            towerofhanoi(n - 1, Source, Destination, temp);
+            Console.WriteLine("Move Element: " + n + " from " + Source + " to " + Destination);
+            towerofhanoi(n - 1, temp, Source, Destination);
+
+
+        }
+    }
+}
