f6c30d666385d9a6aaef6ca2a09eb646d95cdd41
pawan227@gmail.com <pawan227@gmail.com>
2015-10-13 16:23:06 -0700

    Sum of Square and Number of Moves in tower of Hanoi.

---
+++ Algorithm/SumofSquare/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program P = new Program();
+            int SumNumber = P.SumofSquareNumber(5);
+
+            Console.Write("Sum of Square Number" + SumNumber);
+        }
+
+        public int SumofSquareNumber( int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return (n * n) + SumofSquareNumber(n-1);
+
+            }
+
+
+
+        }
+    }
+}
--- Algorithm/towerofhanoi/Program.cs
+++ Algorithm/towerofhanoi/Program.cs
@@ -8,10 +8,14 @@ namespace towerofhanoi
 {
     class Program
     {
+        public int NumberofMoves=0;
         static void Main(string[] args)
         {
             Program P = new Program();
-            P.towerofhanoi(5, "Source", "temp", "Destination");
+
+            P.towerofhanoi(10, "Source", "temp", "Destination");
+
+            Console.WriteLine("Total Number of Moves:" + P.NumberofMoves);
         }
 
         public void towerofhanoi(int n, string Source, string temp, string Destination)
@@ -21,7 +25,7 @@ namespace towerofhanoi
                 return;
 
             }
-
+            NumberofMoves++;
             towerofhanoi(n - 1, Source, Destination, temp);
             Console.WriteLine("Move Element: " + n + " from " + Source + " to " + Destination);
             towerofhanoi(n - 1, temp, Source, Destination);
